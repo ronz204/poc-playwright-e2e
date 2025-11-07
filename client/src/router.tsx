44d633a96e050a0252304036bd3ca13bede55cd2
roney ruiz <roneyruizrojas@gmail.com>
2025-11-07 12:40:10 -0600

    chore: remove play route

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,9 +1,7 @@
-import { PlayPage } from "@Modules/Play/PlayPage";
 import { RootPage } from "@Modules/Root/RootPage";
 import { createBrowserRouter } from "react-router";
 
 // https://reactrouter.com/start/data/installation
 export const Router = createBrowserRouter([
   { path: "/", Component: RootPage },
-  { path: "/play", Component: PlayPage },
 ]);

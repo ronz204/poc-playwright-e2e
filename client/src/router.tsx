2c3d727bec08ad390462b0453b4b360b2d6a8b45
roney ruiz <roneyruizrojas@gmail.com>
2025-11-06 23:04:15 -0600

    docs: add react router data mode link

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,6 +1,7 @@
 import { App } from "./App";
 import { createBrowserRouter } from "react-router";
 
+// https://reactrouter.com/start/data/installation
 export const Router = createBrowserRouter([
   {
     path: "/",

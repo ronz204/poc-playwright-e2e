e6efe4bf5be71d9c77e542ddfee373ac4de64503
roney ruiz <roneyruizrojas@gmail.com>
2025-11-07 12:38:44 -0600

    feat: add ts paths for imports

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@Modules": "/src/Modules",
+      "@Atoms": "/src/Components/Atoms",
+      "@Layouts": "/src/Components/Layouts",
+      "@Molecules": "/src/Components/Molecules",
+      "@Organisms": "/src/Components/Organisms",
+    },
+  },
 });

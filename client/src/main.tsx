6fdf8fa644f066482095eae804e80f2e6cf2a85b
roney ruiz <roneyruizrojas@gmail.com>
2025-11-06 22:30:54 -0600

    chore: purge old files

--- client/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import "./index.css"
-import { App } from "./App"
-import { createRoot } from "react-dom/client"
-
-const root = document.getElementById("root");
-const component = (<App></App>);
-
-createRoot(root!).render(component);
